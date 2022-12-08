7519fa0d2e16936c4ae75ebd2ab025f176da0f5f
cezarybednarz <cezary.bednarz@gmail.com>
2022-12-08 18:27:52 +0000

    Fix led gpio

--- color.py
+++ color.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python3
-# NeoPixel library strandtest example
-# Author: Tony DiCola (tony@tonydicola.com)
-#
-# Direct port of the Arduino NeoPixel library strandtest example.  Showcases
-# various animations on a strip of NeoPixels.
 
 import time
 from rpi_ws281x import PixelStrip, Color
@@ -12,8 +7,6 @@ import argparse
 # LED strip configuration:
 LED_COUNT = 300        # Number of LED pixels.
 LED_PIN = 21
-# LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
-# LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10          # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
--- hello-world.js
+++ hello-world.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 80
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Witamy na Chacie!')
 })
 
 app.listen(port, () => {
